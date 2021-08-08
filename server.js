01f6928501850808fee4e60f8b77c8da5f2a659e
henmusta <hendrimusta88@gmail.com>
2021-08-08 22:32:26 +0700

    first

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyparser = require("body-parser")
 const path= require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config({path:'config.env'})
@@ -11,6 +13,9 @@ const PORT = process.env.PORT|| 8080
 
 //request log
 app.use(morgan('tiny'));
+
+//momgodbconnect
+connectDB();
 
 //parse request to bosy-parser
 app.use(bodyparser.urlencoded({extended:true}))
@@ -23,8 +28,7 @@ app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
 app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
 app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+//load routers
+app.use('/',require('./server/routes/router'))
 
 app.listen(3000, ()=>{console.log('Server Berjalan pada http://localhost:3000')});
